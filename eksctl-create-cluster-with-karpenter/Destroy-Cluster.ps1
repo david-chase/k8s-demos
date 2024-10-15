2a769fff4a8e975c6aa44a7fcdcf09b7f443f80e
David Chase <dchase@hotmail.com>
2024-10-15 14:35:27 -0400

    2024.10.15;14:35;Automated sync

--- eksctl-create-cluster-with-karpenter/Destroy-Cluster.ps1
+++ eksctl-create-cluster-with-karpenter/Destroy-Cluster.ps1
@@ -3,6 +3,23 @@ Write-Host ""
 Write-Host ::: Destroy EKS Cluster With Karpenter v1 ::: -ForegroundColor Cyan
 Write-Host ""
 
+# Read in the config file
+$oConfig = Get-Content -Path 'config.ini' | ConvertFrom-StringData
+
+$env:CLUSTER_OWNER = $oConfig.owner
+$env:CLUSTER_PURPOSE = $oConfig.purpose 
+$env:CLUSTER_CREATEDATE = ( Get-Date -format "yyyy.MM.dd" )
+
+# Start a timer
+$oStopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+$oStopWatch.Start()
+$env:KARPENTER_NAMESPACE = $oConfig.karpenternamespace
+$env:KARPENTER_VERSION = $oConfig.karpenterversion
+$env:K8S_VERSION = $oConfig.version
+
+$env:AWS_PARTITION = "aws" # if you are not using standard partitions, you may need to configure to aws-cn / aws-us-gov
+$env:CLUSTER_NAME = $oConfig.clustername
+
 Write-Host Deleting CF stacks -ForegroundColor Cyan
 Write-Host 'aws cloudformation delete-stack --stack-name "Karpenter-'$env:CLUSTER_NAME'"' -ForegroundColor Green
 Write-Host 'aws cloudformation delete-stack --stack-name ( "eksctl-"'$env:CLUSTER_NAME'"-addon-vpc-cni" )' -ForegroundColor Green
@@ -24,3 +41,7 @@ aws iam delete-role --role-name ( "KarpenterNodeRole-" + $env:CLUSTER_NAME )
 
 # This can take a long time, so make a sound so the user know it's complete
 [console]::beep(500,300)
+
+# Stop the timer
+$oStopWatch.Stop()
+Write-Host `nMinutes elapsed: $oStopWatch.Elapsed.Minutes -ForegroundColor Cyan
