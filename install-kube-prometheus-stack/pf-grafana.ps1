09ba879c013ad5ab1e49e6b8c5e87736b9ad606d
David Chase <dchase@hotmail.com>
2024-10-19 12:44:03 -0400

    2024.10.19;12:43;Automated sync

--- install-kube-prometheus-stack/pf-grafana.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process "http://localhost:3001"
-# Start-Process  -filepath "kubectl" -ArgumentList "kubectl port-forward --namespace monitoring svc/prometheus-grafana 3001:80"
-kubectl port-forward --namespace monitoring svc/prometheus-grafana 3001:80
